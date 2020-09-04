24dab81f8e7df2a11e0b457a6ab0a4fd7603888d
Hasitha Kaushan <hkraclh@gmail.com>
2020-09-04 09:25:59 +0530

    Redirect removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { AuthGuard } from './guard/auth.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: '',   redirectTo: '/profile', pathMatch: 'full' }
 ];
 
 @NgModule({
